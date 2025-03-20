b60ec249b9897b194ede71251aba1b2ac9ba13c6
jim <>
2025-03-20 10:56:25 +0800

    no message

--- src/screens/admin/VendorManagementScreen.tsx
+++ src/screens/admin/VendorManagementScreen.tsx
@@ -2,24 +2,27 @@ import React, { useCallback, useEffect, useState } from 'react';
 import {
   View,
   Text,
-  FlatList,
   TouchableOpacity,
   StyleSheet,
   SafeAreaView,
   Alert,
   Image,
+  ScrollView,
 } from 'react-native';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Vendor, fetchAllVendors, deleteVendor } from '@/api/admin/vendorApi'; // 加入刪除 API
+import { Vendor, fetchAllVendors, deleteVendor } from '@/api/admin/vendorApi';
 import { showLoading, hideLoading } from '@/store/loadingSlice';
 import { AppDispatch } from '@/store/store';
 import { useDispatch } from 'react-redux';
+import { Menu, Provider } from 'react-native-paper';
+import Header from '@/component/Header';
 
 const VendorManagementScreen = () => {
   const dispatch = useDispatch<AppDispatch>();
   const [vendors, setVendors] = useState<Vendor[]>([]);
   const navigation = useNavigation();
+  const [visibleMenuId, setVisibleMenuId] = useState<string | null>(null);
 
   const loadVendors = async () => {
     try {
@@ -50,8 +53,7 @@ const VendorManagementScreen = () => {
     }, [])
   );
 
-  // 刪除廠商
-  const handleDelete = (vendorId, vendorName) => {
+  const handleDelete = (uid, vendorName) => {
     Alert.alert('確認刪除', `確定要刪除廠商「${vendorName}」嗎？`, [
       { text: '取消', style: 'cancel' },
       {
@@ -60,14 +62,12 @@ const VendorManagementScreen = () => {
         onPress: async () => {
           try {
             dispatch(showLoading());
-            console.log('AAAAAAAAAA', vendorId);
-
-            const response = await deleteVendor(vendorId);
+            const response = await deleteVendor(uid);
             dispatch(hideLoading());
 
             if (response.success) {
               Alert.alert('成功', '廠商已刪除');
-              loadVendors(); // 重新載入列表
+              loadVendors();
             } else {
               Alert.alert('錯誤', response.message || '刪除失敗');
             }
@@ -81,271 +81,221 @@ const VendorManagementScreen = () => {
   };
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        {/* Header */}
-        <View style={styles.headerContainer}>
-          <Text style={styles.header}>廠商管理</Text>
-        </View>
+    <Provider>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <View style={styles.backgroundImageContainer}>
+            <Image
+              source={require('@/assets/iot-threeBall.png')}
+              resizeMode="contain"
+            />
+          </View>
 
-        {/* 廠商列表 */}
-        <FlatList
-          data={[...vendors, { uid: 'addButton', isAddButton: true }]}
-          keyExtractor={(item) => item.uid}
-          contentContainerStyle={styles.listContainer}
-          windowSize={1}
-          numColumns={2}
-          renderItem={({ item }) =>
-            item.isAddButton ? (
-              <TouchableOpacity
-                style={styles.addTableButton}
-                onPress={() => navigation.navigate('AddVendor')}
-              >
-                <Image
-                  source={require('@/assets/iot-login-logo.png')}
-                  style={styles.tableIcon}
-                />
-                <Text style={styles.addTableText}>新增廠商 +</Text>
-              </TouchableOpacity>
-            ) : (
-              <TouchableOpacity
-                key={item.uid}
-                style={styles.card}
-                onPress={() =>
-                  navigation.navigate('StoreManagementStack', {
-                    screen: 'StoreManagement',
-                    params: { vendor: item }, // 若有要傳資料
-                  })
-                }
-              >
-                <View style={styles.row}>
-                  {/* 左側圖標 */}
+          <View style={styles.headerContainer}>
+            <Header title="廠商管理" onBackPress={() => navigation.goBack()} />
+          </View>
+
+          <View style={styles.contentContainer}>
+            <ScrollView contentContainerStyle={styles.scrollViewContent}>
+              <View style={styles.vendorGrid}>
+                {vendors.map((item) => (
+                  <TouchableOpacity
+                    key={item.uid}
+                    style={styles.vendorCard}
+                    onPress={() =>
+                      navigation.navigate('StoreManagementStack', {
+                        screen: 'StoreManagement',
+                        params: { vendor: item }, // 若有要傳資料
+                      })
+                    }
+                  >
+                    <Image
+                      source={require('@/assets/iot-logo-black.png')}
+                      style={styles.vendorImage}
+                    />
+                    <View style={styles.vendorFooter}>
+                      <Text style={styles.vendorName}>{item.name}</Text>
+                      <View style={styles.vendorActions}>
+                        <Menu
+                          visible={visibleMenuId === item.uid}
+                          onDismiss={() => setVisibleMenuId(null)}
+                          anchor={
+                            <TouchableOpacity
+                              style={styles.actionIconButton}
+                              onPress={() =>
+                                setVisibleMenuId(
+                                  visibleMenuId === item.uid ? null : item.uid
+                                )
+                              }
+                            >
+                              <Icon
+                                name="dots-vertical"
+                                size={20}
+                                color="#FFF"
+                              />
+                            </TouchableOpacity>
+                          }
+                          contentStyle={styles.menuStyle}
+                        >
+                          <Menu.Item
+                            onPress={() => {
+                              setVisibleMenuId(null);
+                              navigation.navigate('AddVendor', {
+                                vendor: item,
+                              });
+                            }}
+                            title="編輯"
+                            leadingIcon="pencil-outline"
+                          />
+                          <Menu.Item
+                            onPress={() => handleDelete(item.uid, item.name)}
+                            title="刪除"
+                            leadingIcon="trash-can-outline"
+                            titleStyle={{ color: 'red' }}
+                          />
+                        </Menu>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                ))}
+
+                <TouchableOpacity
+                  style={styles.addVendorButton}
+                  onPress={() => navigation.navigate('AddVendor')}
+                >
                   <Image
-                    source={require('@/assets/iot-login-logo.png')}
-                    style={styles.cardIcon}
+                    source={require('@/assets/iot-logo-white.png')}
+                    style={styles.addVendorImage}
                   />
-
-                  {/* 右側信息 */}
-
-                  <View style={{ flex: 1, flexDirection: 'column' }}>
-                    <Text style={styles.cardTitle}>{item.name}</Text>
-                    <Text style={styles.cardSubtitle}>{item.contactInfo}</Text>
+                  <View style={styles.addVendorFooter}>
+                    <Text style={styles.addVendorText}>新增廠商</Text>
+                    <View style={styles.addVendorIcon}>
+                      <Icon name="plus" size={20} color="#FFF" />
+                    </View>
                   </View>
-                </View>
-                {/* 設定按鈕 */}
-                <TouchableOpacity
-                  style={styles.settingsButton}
-                  onPress={() =>
-                    navigation.navigate('AddVendor', { vendor: item })
-                  }
-                >
-                  <Text style={styles.settingsButtonText}>編輯</Text>
-                  <Icon
-                    name="chevron-right"
-                    size={20}
-                    color="#FFF"
-                    style={styles.arrowIcon}
-                  />
                 </TouchableOpacity>
-
-                <TouchableOpacity
-                  style={styles.deleteButton}
-                  onPress={() => handleDelete(item.uid, item.name)}
-                >
-                  <Text style={styles.deleteButtonText}>刪除</Text>
-                  <Icon
-                    name="delete"
-                    size={20}
-                    color="#f23857"
-                    style={styles.deleteButtonIcon}
-                  />
-                </TouchableOpacity>
-              </TouchableOpacity>
-            )
-          }
-        />
-      </View>
-    </SafeAreaView>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      </SafeAreaView>
+    </Provider>
   );
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#F4F8FB',
   },
   container: {
     flex: 1,
-    paddingHorizontal: 20,
-    paddingTop: 20,
   },
-  headerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-  addButton: {
-    backgroundColor: '#007bff',
-    width: 48,
-    height: 48,
-    borderRadius: 24,
+  backgroundImageContainer: {
+    position: 'absolute',
+    right: -200,
+    bottom: 0,
+    zIndex: 2,
     alignItems: 'center',
     justifyContent: 'center',
-    elevation: 5,
-    shadowColor: '#000',
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
+    opacity: 0.1,
   },
-
-  vendorItem: {
-    backgroundColor: '#ffffff',
-    padding: 16,
-    borderRadius: 12,
-    marginBottom: 12,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowRadius: 6,
-    elevation: 3,
+  headerContainer: {
+    backgroundColor: '#FFFFFF',
   },
-  vendorInfoContainer: {
+  contentContainer: {
     flex: 1,
+    padding: 20,
+    zIndex: 3,
   },
-  vendorInfo: {
-    flex: 1,
-  },
-  vendorName: {
-    fontSize: 18,
-    fontWeight: '600',
-    color: '#333',
-  },
-  vendorContact: {
-    fontSize: 14,
-    color: '#666',
-    marginTop: 4,
-  },
-  actionButtons: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  iconButton: {
-    padding: 8,
-  },
-  listContainer: {
+  scrollViewContent: {
     paddingBottom: 20,
   },
-  poolTableItem: {
+  vendorGrid: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
   },
-  poolTableInfoContainer: {
-    flex: 1,
-  },
-  poolTableInfo: {
-    flex: 1,
-  },
-  poolTableName: {
-    fontSize: 18,
-    fontWeight: '600',
-    color: '#333',
-  },
-  poolTableStatus: {
-    fontSize: 14,
-    fontWeight: '500',
-    marginTop: 4,
-  },
-  statusAvailable: {
-    color: 'green',
-  },
-  statusUnavailable: {
-    color: 'red',
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  cardIcon: {
-    width: 40,
-    height: 40,
-    marginRight: 10, // 圖標和桌台名稱間距
-  },
-  cardTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#FFF',
-  },
-  settingsButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-end', // 將按鈕內容靠右
-  },
-  settingsButtonText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#FFF',
-  },
-  arrowIcon: {
-    marginLeft: 5, // 與文字保持距離
-  },
-  addTableButton: {
-    backgroundColor: '#c7dbee',
-    width: '49%',
-    height: 128,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
-  },
-  tableIcon: {
-    width: 50,
-    height: 50,
-    marginBottom: 10,
-  },
-  addTableText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  card: {
-    backgroundColor: '#4787C7',
+  vendorCard: {
+    backgroundColor: '#fff',
     width: '48%',
     height: 128,
-    aspectRatio: 1.5,
     borderRadius: 20,
-    padding: 10,
+    padding: 14,
     justifyContent: 'space-between',
-    marginBottom: 4,
+    marginBottom: 8,
     shadowOpacity: 0.1,
     shadowRadius: 10,
     marginHorizontal: '1%',
   },
-  cardSubtitle: {
-    fontSize: 14,
-    color: '#DDD',
-    marginTop: 4,
+  vendorImage: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    objectFit: 'contain',
   },
-  deleteButton: {
+  vendorFooter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 12,
+  },
+  vendorName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  vendorActions: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-end', // 將按鈕內容靠右
   },
-  deleteButtonText: {
-    fontSize: 14,
+  actionIconButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: '#595858',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 8,
+  },
+  addVendorButton: {
+    backgroundColor: '#FFD700',
+    width: '48%',
+    height: 128,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 14,
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+  },
+  addVendorImage: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    objectFit: 'contain',
+  },
+  addVendorFooter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    marginTop: 12,
+  },
+  addVendorText: {
+    fontSize: 16,
     fontWeight: 'bold',
-    color: '#f23857',
   },
-  deleteButtonIcon: {
-    marginLeft: 5, // 與文字保持距離
+  addVendorIcon: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: '#595858',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  menuStyle: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    elevation: 4,
   },
 });
 
