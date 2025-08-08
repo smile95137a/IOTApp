9b006048e689b5bc3f8c873321d1319f5b299a55
jim <>
2025-08-08 11:42:49 +0800

    no message

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { frontRoutes } from './frontRouter';
-import { adminRoutes } from './adminRouter';
 import { useAuthAdminStore } from '@/stores/authAdminStore';
 import { useAuthFrontStore } from '@/stores/authFrontStore';
 
-const routes = [...frontRoutes, ...adminRoutes];
+const routes = [...frontRoutes];
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,16 +14,9 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  const isAdminRoute = to.path.startsWith('/admin');
-
   if (to.meta.requiresAuth) {
-    if (isAdminRoute) {
-      const adminAuth = useAuthAdminStore();
-      if (!adminAuth.isLogin) return next('/admin/login');
-    } else {
-      const userAuth = useAuthFrontStore();
-      if (!userAuth.isLogin) return next('/login');
-    }
+    const userAuth = useAuthFrontStore();
+    if (!userAuth.isLogin) return next('/login');
   }
 
   next();
